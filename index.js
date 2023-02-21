7bb9dad3c599ac73cc32b98d3e01a1e8c5858549
James Fields <fieldsjay13@gmail.com>
2023-02-21 16:21:30 -0700

    lab complete

--- index.js
+++ index.js
@@ -1,30 +1,39 @@
 // Code your solution in this file!
-function distanceFromHqInBlocks(blocks){
-    if (blocks > 42){
+function distanceFromHqInBlocks(blocks) {
+    if (blocks > 42) {
         return blocks - 42;
-    } else {
+    } 
+    else {
         return 42 - blocks;
     }
 }
-function distanceFromHqInFeet(blocks){
+
+function distanceFromHqInFeet(blocks) {
     return distanceFromHqInBlocks(blocks) * 264;
 }
-function distanceTravelledInFeet(start, destination){
-    if (start < destination){
-        return (destination - start) * 264;
-    } else {
-        return (start - destination) * 264;
+
+function distanceTravelledInFeet(start, end) {
+    if (start < end){
+        return (end - start) * 264;
+    }
+    else {
+        return (start - end) * 264;
     }
 }
-function calculatesFarePrice(start, destination){
-    const distance = distanceTravelledInFeet(start, destination);
-    if (distance <= 400){
+
+function calculatesFarePrice (start, end) {
+    const distance = distanceTravelledInFeet(start, end);
+    
+    if (distance <= 400) {
         return 0;
-    } else if (distance > 400 && distance <= 2000){
-        return .02 * (distance - 400);
-    } else if (distance > 2000 && distance < 2500){
+    }
+    else if (distance > 400 && distance <= 2000) {
+        return (distance - 400) * .02;
+    }
+    else if (distance > 2000 && distance <= 2500) {
         return 25;
-    } else {
-        return 'cannot travel that far';
+    }
+    else {
+        return "cannot travel that far";
     }
 }
